7cdc068418d5b03b4b0e43cac0e02add550ea770
JugDaSlug <knottjaronjr@gmail.com>
2025-01-09 17:12:16 -0500

    Custom animation implementation
    Animations

---
+++ src/shared/AnimationModule.ts
@@ -0,0 +1,33 @@
+import { Players } from "@rbxts/services";
+import AnimationLibrary from "./AnimationLibrary";
+
+interface AnimationModulePreface {
+	SyncPlayer(MethodTable: object): object; //Gives the script access to the players current animate script instance//
+	GetAnimation(): void; //Searches for an animation within the library//
+	LoadAnimation(): void; //Loads an animation onto an animator and returns the animation track if successful//
+	PlayAnimation(): void; //Plays an animation on an animator and returns the animation track if successful//
+}
+
+const AnimationModule: AnimationModulePreface = {} as AnimationModulePreface;
+let AnimateScript: undefined | unknown;
+
+AnimationModule.SyncPlayer = function (MethodTable: object | undefined) {
+	if (typeIs(MethodTable, "table") === true) AnimateScript = MethodTable;
+	else MethodTable = undefined;
+
+	return AnimationLibrary.AnimationPacks.Default;
+};
+
+AnimationModule.GetAnimation = function () {
+	return;
+};
+
+AnimationModule.LoadAnimation = function () {
+	return;
+};
+
+AnimationModule.PlayAnimation = function () {
+	return;
+};
+
+export = AnimationModule;
