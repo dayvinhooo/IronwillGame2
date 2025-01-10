cf96272cfa6dd576e43d08c9b32fdd7347d7a097
JugDaSlug <knottjaronjr@gmail.com>
2025-01-10 03:50:40 -0500

    AnimationPack Switching Added
    Press Q to seamlessly switch between the default and classic animation packs

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,3 +1,15 @@
-// import { makeHello } from "shared/module";
+import { ContextActionService, ReplicatedStorage } from "@rbxts/services";
+import AnimationModule from "shared/AnimationModule";
 
-// print(makeHello("main.client.ts"));
+print(AnimationModule, "WORKINGGGASDSADKLFLJSADKFJDSA");
+
+let toggle = false;
+
+ContextActionService.BindAction("Switch", (name: string, inputState: Enum.UserInputState)=>{
+    if (inputState !== Enum.UserInputState.Begin) return;
+
+    if (toggle) AnimationModule.ApplyAnimationPack("Default");
+    else AnimationModule.ApplyAnimationPack("Classic");
+
+    toggle = !toggle;
+}, false, Enum.KeyCode.Q)
--- src/shared/AnimationModule.ts
+++ src/shared/AnimationModule.ts
@@ -1,23 +1,38 @@
 import { Players } from "@rbxts/services";
 import AnimationLibrary from "./AnimationLibrary";
-
+import Object from "@rbxts/object-utils";
 
 interface AnimationModulePreface {
-	SyncPlayer(MethodTable: object): LuaTuple<[object, object]>; //Gives the script access to the players current animate script instance//
+	SyncPlayer(AnimationTable: object, methods: Record<string, Callback>): LuaTuple<[object, object]>; //Gives the script access to the players current animate script instance//
+	ApplyAnimationPack(packName: string): void; //Changes, adds, or removes various animations for player movement and actions//
 	GetAnimation(): void; //Searches for an animation within the library//
 	LoadAnimation(): void; //Loads an animation onto an animator and returns the animation track if successful//
 	PlayAnimation(): void; //Plays an animation on an animator and returns the animation track if successful//
 }
 
 const AnimationModule: AnimationModulePreface = {} as AnimationModulePreface;
-let AnimateScript: undefined | unknown;
+let animTable: object | undefined = {};
+let methodsTable: Record<string, Callback> | undefined = {};
 
-AnimationModule.SyncPlayer = function (MethodTable: object | undefined) {
-	if (typeIs(MethodTable, "table") === true) AnimateScript = MethodTable;
-	else MethodTable = undefined;
+AnimationModule.SyncPlayer = function (animationTable: object, methods: Record<string, Callback>) {
+	if (typeIs(animationTable, "table") === true) animTable = animationTable;
+	else animTable = undefined;
+
+	if (typeIs(methods, "table") === true) methodsTable = methods;
+	else methodsTable = undefined;
 
 	return $tuple(AnimationLibrary.AnimationPacks.Default, AnimationLibrary.AnimationPacks.Emotes);
 };
+
+AnimationModule.ApplyAnimationPack = function(this: object, packName: string){
+	if (AnimationLibrary.AnimationPacks[packName] === undefined) return;
+	if (methodsTable === undefined) return;
+	if (!(Object.keys(methodsTable).includes("configureAnimationSet") && Object.keys(methodsTable).includes("stopAllAnimations"))) return;
+
+	const pack = AnimationLibrary.AnimationPacks[packName];
+	for (const i of Object.keys(pack)) methodsTable.configureAnimationSet(i, pack[i]);
+	methodsTable.stopAllAnimations(true);
+}
 
 AnimationModule.GetAnimation = function () {
 	return;
